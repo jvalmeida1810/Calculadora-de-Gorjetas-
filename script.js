9a4d10a1c9f2a36eac3dfc71414a9b8c0d8fd638
João Vitor Almeida <joaovitoralmeida1101@gmail.com>
2023-03-11 21:13:31 -0300

    adicionando js ao projeto

---
+++ script.js
@@ -0,0 +1,16 @@
+function calculateTip(event) {
+    event.preventDefault();
+    console.log(event)
+    let bill = document.getElementById('bill').value;
+    let serviceQuality = document.getElementById('serviceQuality').value;
+    let numOfPeople = document.getElementById('people').value;
+
+    if (bill = '' | serviceQuality == 0) {
+        alert("por favor, preencha os valores")
+        return;
+    }
+}
+document.getElementById('totalTip').style.display = "none";
+document.getElementById('each').style.display = "none";
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
