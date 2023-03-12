f3aa709644b01781ec090288901b41389e8e5bfc
JoaoVanzuita <joaog17vanzuita@gmail.com>
2023-03-11 21:51:45 -0300

    fix validation for submit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function calculateTip(event) {
-    event.preventDefault();
-    console.log(event)
-    let bill = document.getElementById('bill').value;
-    let serviceQuality = document.getElementById('serviceQuality').value;
-    let numOfPeople = document.getElementById('people').value;
+  event.preventDefault();
+  // console.log(event)
 
-    if (bill = '' | serviceQuality == 0) {
-        alert("por favor, preencha os valores")
-        return;
-    }
+  let bill = document.getElementById('bill').value;
+  let serviceQuality = document.getElementById('serviceQuality').value;
+  let numOfPeople = document.getElementById('people').value;
+
+  console.log(serviceQuality)
+
+  if (bill == '' || serviceQuality == 0) {
+    alert("por favor, preencha os valores")
+    return;
+  }
 }
 document.getElementById('totalTip').style.display = "none";
 document.getElementById('each').style.display = "none";
